537477a643e2967a0b4f58010100d26683ca02ae
KPatel008 <122328545+KPatel008@users.noreply.github.com>
2025-03-08 09:25:20 -0500

    Update at 'Sat Mar 08 09:25:20 EST 2025'

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -22,7 +22,7 @@ public class Elevator extends SubsystemBase {
   private ElevatorIOInputsAutoLogged m_inputs = new ElevatorIOInputsAutoLogged();
   private final ElevatorIO m_io;
   // GSD setpoints
-  private final double L1Setpoint = 29;
+  private final double L1Setpoint = 29.48;
   private final double L2Setpoint = 34;
   private final double L3Setpoint = 51;
   private final double L4Setpoint = 76
