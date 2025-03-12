3494cd2371664908a99f45db0755a43043b33c78
KPatel008 <122328545+KPatel008@users.noreply.github.com>
2025-03-11 20:40:41 -0400

    fix elevator algae and algae pivot setpoints

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -27,8 +27,8 @@ public class Elevator extends SubsystemBase {
   private final double L3Setpoint = 51;
   private final double L4Setpoint = 77
   ;
-  private final double L2ASetpoint = 48.5;
-  private final double L3ASetpoint = 64.5;
+  private final double L2ASetpoint = 48.5+3+1;
+  private final double L3ASetpoint = 66.5+1.5;
 
   // private final double L1Setpoint = 29;
   // private final double L2Setpoint = 33;
