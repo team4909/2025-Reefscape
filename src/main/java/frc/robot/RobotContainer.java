6246bf59fca9a34295148deb08a572c8fe1feed8
LTeixeira4909 <119272952+LTeixeira4909@users.noreply.github.com>
2025-03-25 15:03:21 -0400

    Update at 'Tue Mar 25 15:03:21 EDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -224,7 +224,7 @@ public class RobotContainer {
         joystick.leftStick().onTrue(s_Climber.winchedPosition());
 
         joystick.b().onTrue(s_Elevator.goToL1()).onFalse(s_Elevator.stop());
-        joystick.leftBumper().onTrue(s_Elevator.goToL2()).onFalse(s_Elevator.goToL1());
+        joystick.leftBumper().onTrue(s_Elevator.goToL2());//.onFalse(s_Elevator.goToL1());
         joystick.rightBumper().onTrue(s_Elevator.goToL3()).onFalse(s_Elevator.goToL1());
         joystick.leftTrigger().onTrue(s_Elevator.goToL4()).onFalse(s_Elevator.goToL1());
         joystick.povUp().onTrue(s_Climber.climbPosition());
