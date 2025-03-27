3fd43a5498e7bc9768be5c16b501727789c659fb
LTeixeira4909 <119272952+LTeixeira4909@users.noreply.github.com>
2025-03-27 19:06:40 -0400

    Update at 'Thu Mar 27 19:06:39 EDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -306,9 +306,9 @@ public class RobotContainer {
         //         new Pose2d(7.495, 5.026, Rotation2d.fromDegrees(-90)), joystick));
 
         
-
-        var startPose = !isBlueAlliance() ? new Pose2d(7.495, 5.026, Rotation2d.fromDegrees(-90))
-        : rotatePoseAboutFieldCenter(startPose, new Pose2d(VisionConstants.aprilTagLayout.getFieldLength()/2, VisionConstants.aprilTagLayout.getFieldWidth()/2, null));
+        var blueStartPose = new Pose2d(7.495, 5.026, Rotation2d.fromDegrees(-90));
+        var startPose = !isBlueAlliance() ? blueStartPose
+        : rotatePoseAboutFieldCenter(blueStartPose, new Pose2d(VisionConstants.aprilTagLayout.getFieldLength()/2, VisionConstants.aprilTagLayout.getFieldWidth()/2, null));
         
         var goToClimbStartPose = new DriveToFieldPose(drivetrain,startPose, joystick, 3);
 
