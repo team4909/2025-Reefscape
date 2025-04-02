8bde7dca7b61ba6c055b6bafbb8338d4f60ddee3
LTeixeira4909 <119272952+LTeixeira4909@users.noreply.github.com>
2025-04-01 20:22:05 -0400

    Update at 'Tue Apr 01 20:22:05 EDT 2025'

--- src/main/java/frc/robot/subsystems/algae/Algae.java
+++ src/main/java/frc/robot/subsystems/algae/Algae.java
@@ -41,7 +41,7 @@ public class Algae extends SubsystemBase {
   }
 
   public Command slowShoot() {
-    return this.runOnce(() -> m_io.setShootVoltage(1)).withName("Intake");
+    return this.runOnce(() -> m_io.setShootVoltage(0)).withName("Intake");
   }
 
   public Command stopShooter() {
