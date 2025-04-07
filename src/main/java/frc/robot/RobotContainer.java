c6b58e3088a1a0737b599bda418b1274ac6415aa
LTeixeira4909 <119272952+LTeixeira4909@users.noreply.github.com>
2025-04-07 19:05:38 -0400

    Update at 'Mon Apr 07 19:05:38 EDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -285,7 +285,7 @@ public class RobotContainer {
                 .onFalse(new InstantCommand(()->joystick.setRumble(RumbleType.kBothRumble, 0)).andThen(s_Shooter.stop()));
 
         joystick.x().whileTrue(Commands.parallel(new DriveToPose(drivetrain,
-                new Transform2d(Units.inchesToMeters(-33.5/2+0.2\[]5), Units.inchesToMeters(13.5+2.25),
+                new Transform2d(Units.inchesToMeters(-33.5/2+0.25), Units.inchesToMeters(13.5+2.25),
                  new Rotation2d()), joystick),s_Shooter.shoot())).onFalse(new InstantCommand(()->joystick.setRumble(RumbleType.kBothRumble, 0))
                  .andThen(s_Shooter.stop()));
 
