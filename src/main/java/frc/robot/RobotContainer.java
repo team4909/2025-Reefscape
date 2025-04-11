6d7ea132ba0e1879fc12b019257507d955b06639
LTeixeira4909 <119272952+LTeixeira4909@users.noreply.github.com>
2025-04-11 19:40:10 -0400

    Update at 'Fri Apr 11 19:40:10 EDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -236,7 +236,7 @@ public class RobotContainer {
         joystick.rightBumper().whileTrue(s_Elevator.goToL3().repeatedly());//.onFalse(s_Elevator.goToL1());
         joystick.leftTrigger().whileTrue(s_Elevator.goToL4().repeatedly());//.onFalse(s_Elevator.goToL1());
         joystick.povUp().onTrue(s_Climber.climbPosition());
-        joystick.povDown().whileTrue(Commands.sequence(s_Elevator.GroundIntake_Wait(), Commands.parallel(s_Elevator.GroundIntake().repeatedly(), Commands.sequence(s_Algae.groundIntake(), s_Algae.intake())))).onFalse(Commands.sequence(Commands.parallel(s_Algae.groundIntakeHome(),s_Elevator.L2_Wait()), s_Algae.reZero()));
+        joystick.povDown().whileTrue(Commands.sequence(s_Elevator.GroundIntake_Wait(), Commands.parallel(s_Elevator.GroundIntake().repeatedly(), Commands.sequence(s_Algae.groundIntake(), s_Algae.intake())))).onFalse(Commands.sequence(Commands.parallel(s_Algae.groundIntakeHome(),s_Elevator.L2_Wait()), new WaitCommand(1),s_Algae.reZero()));
         //joystick.povDown().whileTrue(s_Elevator.moveDown()).onFalse(s_Elevator.stop());
         zeroController.x().onTrue(s_Elevator.reZero());
 
