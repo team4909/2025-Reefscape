51e52c8d3e89db29b5acf2c1859470b8453207f6
LTeixeira4909 <119272952+LTeixeira4909@users.noreply.github.com>
2025-04-11 19:33:19 -0400

    Update at 'Fri Apr 11 19:33:19 EDT 2025'

--- src/main/java/frc/robot/subsystems/algae/Algae.java
+++ src/main/java/frc/robot/subsystems/algae/Algae.java
@@ -79,7 +79,7 @@ public class Algae extends SubsystemBase {
   public Command groundIntakeHome() {
     // return this.run(() -> m_io.gotosetpoint(L1Setpoint,m_gearRatio));
     return this.runOnce(() -> {
-      m_io.gotosetpointslow(DownPosition, m_gearRatio, -5d);
+      m_io.gotosetpointslow(DownPosition, m_gearRatio, -1d);
     }).withName("Down");
   }
 
