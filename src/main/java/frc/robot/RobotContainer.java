fb4d1fef682360732115afda403591235f8ba7d0
LTeixeira4909 <119272952+LTeixeira4909@users.noreply.github.com>
2025-04-16 16:27:05 -0400

    Update at 'Wed Apr 16 16:27:04 EDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -119,8 +119,7 @@ public class RobotContainer {
         NamedCommands.registerCommand("Algae Shoot", s_Algae.shoot().withTimeout(1));
         NamedCommands.registerCommand("Algae Stop", s_Algae.stopShooter().withTimeout(0.1));
         NamedCommands.registerCommand("autoalign left", (Commands.parallel(
-                new DriveToPose(drivetrain, new Transform2d(Units.inchesToMeters(-33.5/2+0.25), Units.inchesToMeters(0.5+2.25-.5), new Rotation2d()), joystick), 
-                s_Shooter.shoot())));
+                new DriveToPose(drivetrain, new Transform2d(Units.inchesToMeters(-33.5/2+0.25), Units.inchesToMeters(0.5+2.25-.5), new Rotation2d()), joystick))));
 
         NamedCommands.registerCommand("autoalign right", Commands.parallel(
         new DriveToPose(drivetrain, new Transform2d(Units.inchesToMeters(-33.5/2+0.75), Units.inchesToMeters(0.5+2.25), new Rotation2d()), joystick)));
