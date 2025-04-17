8e9dbf52e981431d6a97ed078b7ab3032ff1bc45
LTeixeira4909 <119272952+LTeixeira4909@users.noreply.github.com>
2025-04-17 19:19:46 -0400

    Update at 'Thu Apr 17 19:19:45 EDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -307,7 +307,7 @@ public class RobotContainer {
         // auto align right
         // positive moves right for second param of translation
         joystick.b().whileTrue(Commands.parallel(
-                        new DriveToPose(drivetrain, new Transform2d(Units.inchesToMeters(-33.5/2+0.75), Units.inchesToMeters(0.5+2.25-2), new Rotation2d()), joystick, 8), 
+                        new DriveToPose(drivetrain, new Transform2d(Units.inchesToMeters(-33.5/2), Units.inchesToMeters(0.5+2.25), new Rotation2d()), joystick, 8), 
                         s_Shooter.shoot()
                 ))
                 .onFalse(new InstantCommand(()->joystick.setRumble(RumbleType.kBothRumble, 0))
