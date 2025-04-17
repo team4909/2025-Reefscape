d67923325e177456638fa6e1ad1450e87d467e66
LTeixeira4909 <119272952+LTeixeira4909@users.noreply.github.com>
2025-04-17 19:07:13 -0400

    Update at 'Thu Apr 17 19:07:13 EDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -120,10 +120,10 @@ public class RobotContainer {
         NamedCommands.registerCommand("Algae Shoot", s_Algae.shoot().withTimeout(1));
         NamedCommands.registerCommand("Algae Stop", s_Algae.stopShooter().withTimeout(0.1));
         NamedCommands.registerCommand("autoalign left", (Commands.parallel(
-                new DriveToPose(drivetrain, new Transform2d(Units.inchesToMeters(-33.5/2+0.25), Units.inchesToMeters(0.5+2.25-.5), new Rotation2d()), joystick))));
+                new DriveToPose(drivetrain, new Transform2d(Units.inchesToMeters(-33.5/2+0.25), Units.inchesToMeters(0.5+2.25-.5), new Rotation2d()), joystick, 6))));
 
         NamedCommands.registerCommand("autoalign right", Commands.parallel(
-        new DriveToPose(drivetrain, new Transform2d(Units.inchesToMeters(-33.5/2+0.75), Units.inchesToMeters(0.5+2.25), new Rotation2d()), joystick)));
+        new DriveToPose(drivetrain, new Transform2d(Units.inchesToMeters(-33.5/2+0.75), Units.inchesToMeters(0.5+2.25), new Rotation2d()), joystick, 6)));
 
         m_chooser = AutoBuilder.buildAutoChooser();
         SmartDashboard.putData("Auto Chooser", m_chooser);
