9a47cd9c4bcf463c63f9027b434e6f6b00f64e2d
LTeixeira4909 <119272952+LTeixeira4909@users.noreply.github.com>
2025-04-17 14:54:49 -0400

    Update at 'Thu Apr 17 14:54:48 EDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -244,7 +244,7 @@ public class RobotContainer {
                 s_Shooter.shoot(), 
                 s_Shooter.slowShoot(), 
                 () -> s_Elevator.isAtL4()
-        )).onFalse(Commands.parallel(Commands.sequence(new WaitCommand(.25), ), s_Shooter.stop()));
+        )).onFalse(Commands.parallel(Commands.sequence(new WaitCommand(.25), driveWithJoystick()), s_Shooter.stop()));
     
 
         // joystick.y().whileTrue(s_Shooter.slowShoot()).onFalse(s_Shooter.stop());
