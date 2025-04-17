65d09ff45163137ab531a9d5f22c65204047f5db
LTeixeira4909 <119272952+LTeixeira4909@users.noreply.github.com>
2025-04-17 15:26:55 -0400

    Update at 'Thu Apr 17 15:26:55 EDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -318,7 +318,7 @@ public class RobotContainer {
 
         // auto align left
         joystick.x().whileTrue(Commands.parallel(new DriveToPose(drivetrain,
-                new Transform2d(Units.inchesToMeters(-33.5/2+0.25+5), Units.inchesToMeters(13.5+2.25),
+                new Transform2d(Units.inchesToMeters(-33.5/2+0.25+1), Units.inchesToMeters(13.5+2.25),
                 new Rotation2d()), joystick),s_Shooter.shoot())).onFalse(new InstantCommand(()->joystick.setRumble(RumbleType.kBothRumble, 0))
                 .andThen(Commands.parallel(s_Shooter.stop(), new RunCommand(() -> stopDrive(), drivetrain))));
 
