cb96ef883f4b4b770e634bf575326fc9d6ec7fdd
LTeixeira4909 <119272952+LTeixeira4909@users.noreply.github.com>
2025-04-18 12:22:08 -0400

    Update at 'Fri Apr 18 12:22:08 EDT 2025'

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -37,7 +37,7 @@ public class Elevator extends SubsystemBase {
   private final double L1Setpoint = 29; //29.48;
   private final double L2Setpoint = 36;
   private final double L3Setpoint = 52;
-  private final double L4Setpoint = 77.25;
+  private final double L4Setpoint = 77.5;
 
   private final double L2ASetpoint = 48.5+3+1;
   private final double L3ASetpoint = 66.5+1.5;
