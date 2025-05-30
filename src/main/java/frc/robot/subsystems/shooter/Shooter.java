cacb66d2b5d3e9e8b78c7a15af60f608f4a098ab
Dhvan <34456470+Dh-Van@users.noreply.github.com>
2025-05-30 19:00:09 -0400

    Update at 'Fri May 30 19:00:08 EDT 2025'

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -33,7 +33,7 @@ public class Shooter extends SubsystemBase {
   }
 
   public Command intake() {
-    return this.run(() -> m_io.setSpeed(1)).withName("Intake");
+    return this.run(() -> m_io.setSpeed(0.8)).withName("Intake");
   }
 
   @Override
