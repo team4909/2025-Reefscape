5bc3438f890f1963d11ae7d5b1390a2821b6a5f1
LTeixeira4909 <119272952+LTeixeira4909@users.noreply.github.com>
2025-06-02 20:00:13 -0400

    Update at 'Mon Jun 02 20:00:13 EDT 2025'

--- src/main/java/frc/robot/subsystems/drivetrain/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/CommandSwerveDrivetrain.java
@@ -440,8 +440,6 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
                                 m_hasAppliedOperatorPerspective = true;
                             });
         }
-
-        if (Gyro.getInstance().isTipping()){
     }
 
     private void startSimThread() {
